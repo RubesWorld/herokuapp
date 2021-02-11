a946b28952ec030c616ddc1439e25deb5efbfea6
RubesWorld <hi@rubesworld.live>
2021-02-11 10:55:11 -0800

    Monolith fix

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const dotenv = require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
+//this is creating a path for heroku to get to 
+app.use(express.static(path.join(__dirname, "client/build", "index.html")));
 
 //APIs don't Receive data, just provide so no need for request. _ is placeholder
 
